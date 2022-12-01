7c03a6c2d87d8d661ca76923e22f69a4e1d91308
Brandon Murry <brandonfossedu@fcboe.org>
2022-12-01 10:40:00 -0500

    Edited ifelse

--- ifelse.c
+++ ifelse.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c;    // Create a 3rd variable 'c'
+    int a, b;    // Create a 3rd variable 'c'
 
-    printf("Enter three integers separated by spaces: ");
-    scanf("%i %i %i", &a, &b, &c);   // Get input for all THREE variables
+    printf("Enter two integers separated by spaces: ");
+    scanf("%i %i", &a, &b);   // Get input for all THREE variables
 
     // Test whether a is equal, greater than, or less than the sum of b and c.
-    if(a == b + c) {
-        printf("%i is equal to the sum of %i and %i", a, b, c);
+    if(a == b) {
+        printf("%i is equal to %i", a, b);
     }
-    else if(a > b + c) {
-        printf("%i is greater than the sum of %i and %i\n", a, b, c);
+    else if(a > b) {
+        printf("%i is greater than %i\n", a, b);
     }
     else {
         printf("%i is less than %i\n", a, b);
